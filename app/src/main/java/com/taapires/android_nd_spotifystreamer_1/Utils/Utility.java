efcc5c4ed0dd7ffef9169783825ebeb443e51ef7
Telmo Pires <telmo.pires@gmail.com>
2015-06-24 14:47:38 +0100

    country codes added

--- app/src/main/java/com/taapires/android_nd_spotifystreamer_1/Utils/Utility.java
+++ app/src/main/java/com/taapires/android_nd_spotifystreamer_1/Utils/Utility.java
@@ -6,6 +6,9 @@ import android.preference.PreferenceManager;
 
 import com.taapires.android_nd_spotifystreamer_1.R;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by telmo on 24/06/15.
  */
@@ -15,5 +18,13 @@ public class Utility {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
         return prefs.getString(context.getString(R.string.pref_location_key),
                 context.getString(R.string.pref_location_default));
+    }
+
+    public static List<String> getCountryCodes() {
+        String[] countryCodes = {
+                "AD", "AR", "AT", "AU", "BE", "BG", "BO", "BR", "CA", "CH", "CL", "CO", "CR", "CY", "CZ", "DE", "DK", "DO", "EC", "EE", "ES", "FI", "FR", "GB", "GR", "GT", "HK", "HN", "HU", "IE", "IS", "IT", "LI", "LT", "LU", "LV", "MC", "MT", "MX", "MY", "NI", "NL", "NO", "NZ", "PA", "PE", "PH", "PL", "PT", "PY", "RO", "SE", "SG", "SI", "SK", "SV", "TR", "TW", "US", "UY"
+        };
+
+        return Arrays.asList(countryCodes);
     }
 }
